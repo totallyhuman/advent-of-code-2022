4ed13243273a97f8923907a0df8accf84d0a58ff
totallyhuman <bsumantb@gmail.com>
2022-12-02 01:01:08 -0500

    Clean up day 1

--- 01/01.py
+++ 01/01.py
@@ -1,12 +1,12 @@
 def part1(x):
-    return max(sum(map(int, i.splitlines())) for i in x)
+    return max(sum(i) for i in x)
 
 def part2(x):
-    return sum(sorted(sum(map(int, i.splitlines())) for i in x)[-3:])
+    return sum(sorted(sum(i) for i in x)[-3:])
 
 with open('01.in') as f:
     s = f.read()
-    x = s.split('\n\n')
+    x = [[*map(int, i.splitlines())] for i in s.split('\n\n')]
 
     print(part1(x))
     print(part2(x))
