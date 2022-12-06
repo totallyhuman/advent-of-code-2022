bb63aec87cbe69b2336ccbedd583ce5039be797a
totallyhuman <bsumantb@gmail.com>
2022-12-06 00:35:38 -0500

    Add day 6 Well, that was suspiciously easy.

---
+++ 06/06.py
@@ -0,0 +1,20 @@
+def part1(x):
+    for i in range(4, len(x) + 1):
+        p = x[i - 4:i]
+
+        if len({*p}) == 4:
+            return i
+
+def part2(x):
+    for i in range(14, len(x) + 1):
+        p = x[i - 14:i]
+
+        if len({*p}) == 14:
+            return i
+
+with open('06.in') as f:
+    s = f.read()
+    x = s
+
+    print(part1(x))
+    print(part2(x))
