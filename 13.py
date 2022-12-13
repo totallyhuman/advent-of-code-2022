4f38c3599671bd1d9bf2d38c47bfb70ea99ed75e
totallyhuman <bsumantb@gmail.com>
2022-12-13 00:51:28 -0500

    Add day 13

---
+++ 13.py
@@ -0,0 +1,50 @@
+import functools
+
+def compare(x, y):
+    for xi, yi in zip(x, y):
+        if type(xi) is int and type(yi) is int:
+            if xi < yi:
+                return -1
+            elif xi > yi:
+                return 1
+        else:
+            if type(xi) is not list:
+                c = compare([xi], yi)
+            elif type(yi) is not list:
+                c = compare(xi, [yi])
+            else:
+                c = compare(xi, yi)
+
+            if c:
+                return c
+    
+    if len(x) < len(y):
+        return -1
+    elif len(x) > len(y):
+        return 1
+    
+    return 0
+
+def part1(x):
+    l = [tuple(map(eval, i.splitlines())) for i in x.split('\n\n')]
+    c = []
+
+    for i, (x, y) in enumerate(l, start = 1):
+        if compare(x, y) == -1:
+            c.append(i)
+    
+    return sum(c)
+
+def part2(x):
+    l = [*map(eval, x.replace('\n\n', '\n').splitlines())] + [[[2]], [[6]]]
+
+    s = sorted(l, key = functools.cmp_to_key(compare))
+
+    return (s.index([[2]]) + 1) * (s.index([[6]]) + 1)
+
+with open('input/13.in') as f:
+    s = f.read()
+    x = s
+
+    print(part1(x))
+    print(part2(x))
